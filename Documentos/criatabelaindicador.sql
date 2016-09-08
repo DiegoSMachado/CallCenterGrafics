1a501bc63dcb207a736f3d01c7d081d1e3686104
DiegoSMachado <diegomachado.designer@gmail.com>
2016-09-08 17:05:22 -0300

    correção SQL

---
+++ Documentos/criatabelaindicador.sql
@@ -0,0 +1,14 @@
+CREATE TABLE indicadores.tb_operadores (
+dt_referencia      VARCHAR
+,cd_colaborador    VARCHAR
+,nm_colaborador    VARCHAR
+,tot_alo           INT
+,tot_cpc           INT
+,tot_venda         INT
+,tot_acionamento   INT
+,alo_acionamento   NUMERIC(14,2)
+,cpc_alo           NUMERIC(14,2)
+,venda_cpc         NUMERIC(14,2)
+,cpc_acionamento   NUMERIC(14,2)
+,venda_acionamento NUMERIC(14,2)
+)
---
+++ Documentos/criatabelaoperador.sql
@@ -0,0 +1,12 @@
+CREATE TABLE indicadores.tb_indicadores (
+dt_referencia VARCHAR(10)
+,hr_referencia VARCHAR(2)
+,tot_alo INT
+,tot_cpc INT
+,tot_venda INT
+,alo_acionamento NUMERIC(14,2)
+,cpc_alo NUMERIC(14,2)
+,venda_cpc NUMERIC(14,2)
+,cpc_acionamento NUMERIC(14,2)
+,venda_acionamento NUMERIC(14,2)
+)
